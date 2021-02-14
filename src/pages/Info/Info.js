417a904e26c55de8ce2cbf829ef24fd36a211388
Barış Ortaç <baris.ortac@hamurlabs.com>
2021-02-14 14:06:57 +0300

    update info

--- src/pages/Info/Info.js
+++ src/pages/Info/Info.js
@@ -7,21 +7,21 @@ const Info = () => {
     <Flex>
       <Flex flexDirection="column" mt="4em" fontSize="l">
         <Divider mt="1em" mb="1em"/>
-        <Text fontWeight="bold" display="block">
+        <Text fontWeight="bold" display="block" fontSize="xl">
           What is this?
         </Text>
         <Text>
           It is a proof-of-concept game that includes <b>React, ChakraUI, MongoDB,
           Express, ContextAPI, Reducer, and Vercel</b> for the deployment.
         </Text>
-        <Text fontWeight="bold">
+        <Text fontWeight="bold" fontSize="xl">
           What is the objective?
         </Text>
         <Text>
-          Find sampled icons in the icon pool. You will get a score according to
+          Find the sampled icons in the icon pool. You will get a score according to
           your finish time & difficulty level.
         </Text>
-        <Text fontWeight="bold">
+        <Text fontWeight="bold" fontSize="xl">
           Can I see your code?
         </Text>
         <Text>
